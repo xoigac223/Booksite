5ba0eaa298a4bad0c41109827001d40173ceaae6
Nguyen Dao Duy Kien <kienlop9altt2014@gmail.com>
2021-06-14 13:23:37 +0700

    admin

---
+++ wwwroot/controller/cart.js
@@ -0,0 +1,71 @@
+function renderCart(item) {
+  const cart = `<tr class="cart-table__item-${item.id}">
+    <td class="product-thumbnail"><a href="single-product.html?id=${item.id}"><img src="images/product/sm-3/1.jpg" alt="product img"></a></td>
+    <td class="product-name"><a href="#">${item.name}</a></td>
+    <td class="product-price"><span class="amount">${item.price} VND</span></td>
+    <td class="product-quantity"><input type="number" min="1" value="${item.amount}" class="product-quantity__input-${item.id} product-quantity__input"></td>
+    <td class="product-subtotal">${item.amount * item.price} VND</td>
+    <td class="product-remove"><a href="#" class="product-${item.id} product-remove__link">X</a></td>
+  </tr>`
+  $('.cart-table__content').append(cart);
+}
+
+function showListCart() {
+  let total = 0;
+  Object.keys(sessionStorage).forEach((key) => {
+    const item = JSON.parse(sessionStorage.getItem(key));
+    total += item.amount * item.price;
+    renderCart(item);
+  });
+  $('.cart__total__amount-price').text(`${total} VND`)
+}
+
+showListCart();
+
+function removeCart(e) {
+  e.preventDefault();
+  const flag = window.confirm('Are you sure you want to delete this item?');
+  if(flag === true) {
+    const id = e.target.classList[0].split('-')[1];
+    sessionStorage.removeItem(id);
+    console.log($(`.miniproduct__${id}`)[0]);
+    $(`.cart-table__item-${id}`).remove();
+    if($(`.miniproduct__${id}`)[0] !== undefined) {
+      $(`.miniproduct__${id}`).remove();
+      let total = 0;
+      Object.keys(sessionStorage).forEach((key) => {
+        const item = JSON.parse(sessionStorage.getItem(key));
+        total += item.amount * item.price;
+      });
+      $('.total_amount-price').text(`${total} VND`)
+      $('.cart__total__amount-price').text(`${total} VND`)
+      $('.item-total__num').text(`${sessionStorage.length} items`);
+    }
+    $('.product_qun').text(sessionStorage.length);
+  }
+}
+
+$(document).ready(function () {
+  $('.product-remove__link').click((e) => {
+    removeCart(e)
+  })
+  $('.product-quantity__input').change((e) => {
+    const id = e.target.classList[0].split('-')[2];
+    let product = JSON.parse(sessionStorage.getItem(id));
+    product.amount = e.target.value
+    $(`.cart-table__item-${id} .product-subtotal`).text(`${product.amount * product.price} VND`);
+    sessionStorage.setItem(id, JSON.stringify(product));
+    console.log(e.target.value + "" + e.target.classList[0].split('-')[2]);
+    if($(`.miniproduct__${id}`)[0] !== undefined) {
+      $(`.miniproduct__${id} .product_prize .qun`).text(`Số lượng: ${product.amount}`)
+      $(`.miniproduct__${id} .content .prize`).text(`${product.amount * product.price} VND`)
+      let total = 0;
+      Object.keys(sessionStorage).forEach((key) => {
+        const item = JSON.parse(sessionStorage.getItem(key));
+        total += item.amount * item.price;
+      });
+      $('.total_amount-price').text(`${total} VND`)
+      $('.cart__total__amount-price').text(`${total} VND`)
+    }
+  })
+})
---
+++ wwwroot/controller/index.js
@@ -0,0 +1,199 @@
+let productList;
+
+function handlerQuickview(e) {
+  $('.modal-body .modal-product').remove();
+  const index = e.target.classList[0].split('-')[1] - 1;
+  let product = productList[index];
+
+  const modalProduct = `<div class="modal-product">
+  <!-- Start product images -->
+  <div class="product-images">
+    <div class="main-image images">
+      <img alt="big images" src="images/product/big-img/1.jpg">
+    </div>
+  </div>
+  <!-- end product images -->
+  <div class="product-info">
+    <h1>${product.name}</h1>
+    <div class="rating__and__review">
+      <ul class="rating">
+        <li><span class="ti-star"></span></li>
+        <li><span class="ti-star"></span></li>
+        <li><span class="ti-star"></span></li>
+        <li><span class="ti-star"></span></li>
+        <li><span class="ti-star"></span></li>
+      </ul>
+      <div class="review">
+        <a href="#">4 customer reviews</a>
+      </div>
+    </div>
+    <div class="price-box-3">
+      <div class="s-price-box">
+        <span class="new-price">${product.price} VND</span>
+      </div>
+    </div>
+    <div class="quick-desc">
+      ${product.description}
+    </div>
+    <div class="social-sharing">
+      <div class="widget widget_socialsharing_widget">
+        <h3 class="widget-title-modal">Share this product</h3>
+        <ul class="social__net social__net--2 d-flex justify-content-start">
+          <li class="facebook"><a href="#" class="rss social-icon"><i class="zmdi zmdi-rss"></i></a></li>
+          <li class="linkedin"><a href="#" class="linkedin social-icon"><i
+                class="zmdi zmdi-linkedin"></i></a></li>
+          <li class="pinterest"><a href="#" class="pinterest social-icon"><i
+                class="zmdi zmdi-pinterest"></i></a></li>
+          <li class="tumblr"><a href="#" class="tumblr social-icon"><i class="zmdi zmdi-tumblr"></i></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="addtocart-btn">
+      <a href="#" class="addtocart-btn__link">Add to cart</a>
+    </div>
+  </div>
+</div>`
+  $('.modal-body').append(modalProduct);
+  $('.addtocart-btn .addtocart-btn__link').click((e) => {
+    e.preventDefault();
+    if(sessionStorage.getItem(product.id) === null) {
+      product.amount = 1;
+      sessionStorage.setItem(product.id, JSON.stringify(product));
+    } else {
+      product = JSON.parse(sessionStorage.getItem(product.id));
+      product.amount++;
+      sessionStorage.setItem(product.id, JSON.stringify(product));
+    }
+    alert('Add success!');
+    window.location.reload();
+  })
+}
+
+
+
+function setListProduct(product1, product2) {
+  const doubleProductItem = `<div class="single__product">
+  <div class="col-lg-3 col-md-4 col-sm-6 col-12">
+    <div class="product product__style--3">
+      <div class="product__thumb">
+        <a class="first__img" href="single-product.html?id=${product1.id}"><img src="images/books/1.jpg" alt="product image"></a>
+        <a class="second__img animation1" href="single-product.html?id=${product1.id}"><img src="images/books/2.jpg" alt="product image"></a>
+        <div class="hot__box">
+          <span class="hot-label">PRODUCT</span>
+        </div>
+      </div>
+      <div class="product__content content--center content--center">
+        <h4><a href="single-product.html?id=${product1.id}">${product1.name}</a></h4>
+        <ul class="prize d-flex">
+          <li>${product1.price} VND</li>
+        </ul>
+        <div class="action">
+          <div class="actions_inner">
+            <ul class="add_to_links">
+              <li><a class="cart" href="cart.html"><i class="bi bi-shopping-bag4"></i></a></li>
+              <li><a class="wishlist" href="#"><i class="bi bi-shopping-cart-full"></i></a></li>
+              <li><a class="compare" href="#"><i class="bi bi-heart-beat"></i></a></li>
+              <li><a data-toggle="modal" title="Quick View" class="quickview-${product1.id} quickview modal-view detail-link" href="#productmodal"><i class="quickview-${product1.id} bi bi-search"></i></a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="product__hover--content">
+          <ul class="rating d-flex">
+            <li class="on"><i class="fa fa-star-o"></i></li>
+            <li class="on"><i class="fa fa-star-o"></i></li>
+            <li class="on"><i class="fa fa-star-o"></i></li>
+            <li><i class="fa fa-star-o"></i></li>
+            <li><i class="fa fa-star-o"></i></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-3 col-md-4 col-sm-6 col-12">
+    <div class="product product__style--3">
+      <div class="product__thumb">
+        <a class="first__img" href="single-product.html?id=${product2.id}"><img src="images/books/3.jpg" alt="product image"></a>
+        <a class="second__img animation1" href="single-product.html?id=${product2.id}"><img src="images/books/9.jpg" alt="product image"></a>
+        <div class="hot__box">
+          <span class="hot-label">PRODUCT</span>
+        </div>
+      </div>
+      <div class="product__content content--center content--center">
+        <h4><a href="single-product.html?id=${product2.id}">${product2.name}</a></h4>
+        <ul class="prize d-flex">
+          <li>${product2.price} VND</li>
+        </ul>
+        <div class="action">
+          <div class="actions_inner">
+            <ul class="add_to_links">
+              <li><a class="cart" href="cart.html"><i class="bi bi-shopping-bag4"></i></a></li>
+              <li><a class="wishlist" href="#"><i class="bi bi-shopping-cart-full"></i></a></li>
+              <li><a class="compare" href="#"><i class="bi bi-heart-beat"></i></a></li>
+              <li><a data-toggle="modal" title="Quick View" class="quickview-${product2.id} quickview modal-view detail-link" href="#productmodal"><i class="quickview-${product2.id} bi bi-search"></i></a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="product__hover--content">
+          <ul class="rating d-flex">
+            <li class="on"><i class="fa fa-star-o"></i></li>
+            <li class="on"><i class="fa fa-star-o"></i></li>
+            <li class="on"><i class="fa fa-star-o"></i></li>
+            <li><i class="fa fa-star-o"></i></li>
+            <li><i class="fa fa-star-o"></i></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>`
+  $('#product__indicator--4__product-list').append(doubleProductItem);
+}
+
+
+$(document).ready(function () {
+  $.get('https://localhost:5001/api/Book', (res) => {
+    productList = res;
+    for (let i = 0; i < productList.length; i += 2) {
+      setListProduct(productList[i], productList[i + 1])
+    }
+    $('.product__indicator--4').owlCarousel({
+      loop: true,
+      margin: 0,
+      nav: true,
+      autoplay: false,
+      autoplayTimeout: 10000,
+      items: 4,
+      navText: ['<i class="zmdi zmdi-chevron-left"></i>', '<i class="zmdi zmdi-chevron-right"></i>'],
+      dots: false,
+      lazyLoad: true,
+      responsive: {
+        0: {
+          items: 1
+        },
+        576: {
+          items: 2
+        },
+        768: {
+          items: 3
+        },
+        992: {
+          items: 4
+        },
+        1920: {
+          items: 4
+        }
+      }
+    });
+    $('.add_to_links .quickview').click((e) => {
+      // console.log('hello');
+      handlerQuickview(e);
+    });
+  });
+})
+
+
+
+// $('#furniture--4__product-list').append(productItem)
+// $('#product__indicator--4__product-list').append(doubleProductItem)
+
