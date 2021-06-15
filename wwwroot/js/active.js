2c05384befc3be50f55ed842577a6a182ccc1e06
Dang <haidangk63@gmail.com>
2021-06-15 16:45:31 +0700

    update 15/6

--- wwwroot/js/active.js
+++ wwwroot/js/active.js
@@ -203,32 +203,7 @@
 
 
   /*=============  Produst Activation  ==========*/
-  $('.productcategory__slide--2').owlCarousel({
-    loop: true,
-    margin: 0,
-    nav: true,
-    autoplay: false,
-    autoplayTimeout: 10000,
-    items: 3,
-    navText: ['<i class="zmdi zmdi-chevron-left"></i>', '<i class="zmdi zmdi-chevron-right"></i>'],
-    dots: false,
-    lazyLoad: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      576: {
-        items: 2
-      },
-      768: {
-        items: 3
-      },
-      1920: {
-        items: 3
-      }
-    }
-  });
-
+  
 
   /*=============  Product Activation ============*/
   // $('.product__indicator--4').owlCarousel({
@@ -613,5 +588,9 @@ $(document).ready(function () {
     })
   })
   $('.shopcart .cartbox_active .product_qun').text(sessionStorage.length);
-
+  $.get('https://localhost:5001/api/Category', (res) => {
+    res.forEach((item) => {
+      $('.megamenu .megamenu__categories').append(`<li><a href="shop-grid.html?category=${item.id}">${item.name}</a></li>`)
+    })
+  })
 });
